61f0e1d771d68f6b17fa3eaefd5b83e985853de6
Edouard Ravelomanantsoa <edravelo@gmail.com>
2023-06-12 12:25:09 +0200

    stupid coach on rails

--- Gemfile
+++ Gemfile
@@ -63,6 +63,7 @@ end
 
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
+  gem "launchy"
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
---
+++ test/system/questions_test.rb
@@ -0,0 +1,38 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit questions_url
+  #
+  #   assert_selector "h1", text: "Questions"
+  # end
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "saying I am going to work yields a great response from the coach" do
+    visit ask_url
+    fill_in "question", with: "I am going to work"
+    click_on "Ask"
+
+    assert_text "Great!"
+  end
+
+  test "asking anything yields a grumpy response form the coach" do
+    visit ask_url
+    fill_in "question", with: "what's up?"
+    click_on "Ask"
+    take_screenshot
+
+    assert_text "Silly question"
+  end
+end
